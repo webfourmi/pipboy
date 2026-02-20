06c27b79490f1f0a8231365e373b272bc5d28df2
webfourmi <134513115+webfourmi@users.noreply.github.com>
2026-02-20 16:07:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,44 @@
-const CACHE = "pipboy-v16";
-const ASSETS = ["./","./index.html","./style.css","./sw.js","./manifest.webmanifest"];
+const CACHE_NAME = "pipboy-v16";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-192.png",
+  "./icons/icon-maskable-512.png",
+  "./icons/favicon.ico"
+];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  const req = event.request;
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match("./index.html")))
+    caches.match(req).then((cached) => {
+      return (
+        cached ||
+        fetch(req).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+          return res;
+        }).catch(() => caches.match("./"))
+      );
+    })
   );
 });
